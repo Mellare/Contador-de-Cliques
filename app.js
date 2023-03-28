2caded59c088b77fc00637ff57dff1f2e38efa47
Mellare <86076128+Mellare@users.noreply.github.com>
2023-03-27 22:30:20 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let valorInicial = 0;
+
+function acrescentar(){
+    valorInicial = valorInicial + 1;
+    window.alert(valorInicial);
+    
+    if(valorInicial == 5){
+      window.alert("Parabéns");
+}
+
+}
+
+function decrescer(){
+    valorInicial = valorInicial - 1;
+    window.alert(valorInicial);
+}
